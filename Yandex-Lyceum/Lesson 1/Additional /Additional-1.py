dabafc8aea1415dc94daae5c14aedba159fbab1f
Weponsv <nicolasgpalchikov@gmail.com>
2025-10-07 22:15:12 +0300

    Linked GitHub with  VS Code on my ThinkPad X260

---
+++ Yandex-Lyceum/Lesson 1/Additional /Additional-1.py
@@ -0,0 +1,7 @@
+gb = 42
+mb = gb * 1024
+kb = mb * 1024
+byte = kb * 1024
+bit = byte * 8
+
+print(f'{gb} Гбайт = {mb} Мбайт = {kb} Кбайт = {byte} байт = {bit} бит')
--- Yandex-Lyceum/Lesson 1/Homework-4.py
+++ Yandex-Lyceum/Lesson 1/Homework-4.py
@@ -1,1 +1,25 @@
-# Еще не сделал
+separator = '''----------
+
+----------'''
+
+main = input()
+
+chapter1_1 = input()
+chapter1_2 = input()
+
+chapter2_1 = input()
+chapter2_2 = input()
+
+####################
+
+print(main)
+
+print('Глава I')
+print(chapter1_1)
+print(chapter1_2)
+
+print(separator)
+
+print('Глава II')
+print(chapter2_1)
+print(chapter2_2)
