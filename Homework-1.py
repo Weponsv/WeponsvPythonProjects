2da84267dac9e7ad7ea9e217e486f0833c0c7414
Weponsv <nicolasgpalchikov@gmail.com>
2025-10-07 20:23:37 +0300

    Add files via upload

---
+++ Homework-1.py
@@ -0,0 +1,6 @@
+day_in_seconds = 60 * 60 * 24
+year_in_seconds = day_in_seconds * 365
+speed_light = 300000
+lenght = 4
+
+print(year_in_seconds * speed_light * lenght)
---
+++ Homework-2.py
@@ -0,0 +1,9 @@
+ball_1 = input()
+ball_2 = input()
+ball_3 = input()
+ball_4 = input()
+
+print(ball_4)
+print(ball_3)
+print(ball_2)
+print(ball_1)
